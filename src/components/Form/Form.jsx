bde32d5dc4ad3789a5839152939cdd058550eb8a
Nikola Lorenc <nikola.skarkova@gmail.com>
2024-06-02 11:53:24 +0200

    přidání homepage a komponenty form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export const Form = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  const handleStartDate = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDate = (date) => {
+    setEndDate(date);
+  };
+
+  const handleCountry = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Vyber zemi:</label>
+          <select
+            id="countries"
+            value={selectedCountry}
+            onChange={handleCountry}
+          >
+            <option value="">Vyberte</option>
+            <option value="anglie">Anglie</option>
+            <option value="belgie">Belgie</option>
+            <option value="spanelsko">Španělsko</option>
+          </select>
+        </div>
+        <div>
+          <label>Zadejte datum:</label>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <ReactDatePicker
+              selected={startDate}
+              onChange={handleStartDate}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="Start Date"
+            />
+            <span style={{ margin: '0 10px' }}>do</span>
+            <ReactDatePicker
+              selected={endDate}
+              onChange={handleEndDate}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              placeholderText="End Date"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,23 @@
+import { useState } from 'react';
+import { Form } from '../../components/Form/Form';
 import './style.css';
 
 export const HomePage = () => {
+  const [isFormVisible, setFormVisible] = useState(false);
+
+  const toggleFormVisibility = () => {
+    setFormVisible(!isFormVisible);
+  };
+
   return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+    <div>
+      <h1>Headline</h1>
+      <h2>Subheadline</h2>
+      <button onClick={toggleFormVisibility}>
+        {isFormVisible ? 'Hide Form' : 'Show Form'}
+      </button>
+
+      {isFormVisible && <Form />}
     </div>
   );
 };
