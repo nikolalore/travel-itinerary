6e86b6574b65b4ec0dbdb8cf70680e91125a65f8
Nikola Lorenc <nikola.skarkova@gmail.com>
2024-06-09 14:53:38 +0200

    přidání fontů, úprava css

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,7 +3,6 @@ import { Form } from '../../components/Form/Form';
 import './style.css';
 import React from 'react';
 
-
 export const HomePage = () => {
   const [isFormVisible, setFormVisible] = useState(false);
 
@@ -12,22 +11,22 @@ export const HomePage = () => {
   };
 
   return (
-    <div className='homepage'>
-    <div className="content">
-      <h1>Naplánujte si svou ideální dovolenou</h1>
-      <h2>
-        Vytvořte si vlastní cestovní itinerář a plánujte snadno a přehledně.
-      </h2>
-      <div id="form">
-        {!isFormVisible && (
-          <button onClick={toggleFormVisibility} className="btn-plan">
-            PLÁNUJ
-          </button>
-        )}
+    <div className="homepage">
+      <div className = {!isFormVisible? "content" : "content content-transition"}>
+        <h1>Naplánujte si svou ideální dovolenou</h1>
+        <h2>
+          Vytvořte si vlastní cestovní itinerář a plánujte snadno a přehledně.
+        </h2>
+        <div id="form">
+          {!isFormVisible && (
+            <button onClick={toggleFormVisibility} className="btn-plan">
+              Plánuj
+            </button>
+          )}
 
-        {isFormVisible && <Form />}
+          {isFormVisible && <Form />}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
