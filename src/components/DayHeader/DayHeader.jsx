242ed0dfd130e735de1c0dc7aabf42691ba9a76f
Nikola Lorenc <nikola.skarkova@gmail.com>
2024-06-12 12:41:04 +0200

    zprovozneni tlacitka zpet u daydetail

--- src/components/DayHeader/DayHeader.jsx
+++ src/components/DayHeader/DayHeader.jsx
@@ -1,21 +1,42 @@
 import './style.css';
 import { Weather } from '../Weather/Weather';
+import { useNavigate, useParams } from 'react-router-dom';
 
+export const DayHeader = ({
+  title,
+  day,
+  coordinatesY,
+  coordinatesX,
+  date,
+  formattedDate,
+}) => {
+  const { tripId } = useParams();
 
-export const DayHeader = ({title, day, coordinatesY, coordinatesX, date, formattedDate }) => {
+  const navigate = useNavigate();
+
+  const toDateResult = () => {
+    navigate(`/trip/${tripId}`);
+  };
+
   return (
     <div className="day-header">
-    <div className="left-section">
-      <button className="back-button">&#8249;</button>
-      <div className="text-container">
-        <div className="title-and-day">
-          <h1 className="title">{title} - </h1>
-          <span className="day">{day}</span>
+      <div className="left-section">
+        <button onClick={toDateResult} className="back-button">
+          &#8249;
+        </button>
+        <div className="text-container">
+          <div className="title-and-day">
+            <h1 className="title">{title} - </h1>
+            <span className="day">{day}</span>
+          </div>
+          <span className="date">{formattedDate}</span>
         </div>
-        <span className="date">{formattedDate}</span>
       </div>
+      <Weather
+        coordinatesY={coordinatesY}
+        coordinatesX={coordinatesX}
+        date={date}
+      />
     </div>
-    <Weather coordinatesY={coordinatesY} coordinatesX={coordinatesX} date={date}/>
-  </div>
   );
 };
